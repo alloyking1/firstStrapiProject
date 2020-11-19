df53758c94f7d2cf629a1a622438d1d584f58da7
alloyking1 <alloyking1@gmail.com>
2020-11-19 08:28:31 -0500

    installation and setting up of strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '66d343acd9de7727c33374ad339af7dd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd82250e4-6b1f-48ee-8230-667e17da24a9'
+};
