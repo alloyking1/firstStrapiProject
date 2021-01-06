53b67bf6fba73be3173f947d52155fc4ce2e196d
alloyking1 <alloyking1@gmail.com>
2021-01-06 11:28:31 -0500

    added ck editor to component

--- plugins/my-custom-plugin/admin/src/components/MyCustomPlugin/index.js
+++ plugins/my-custom-plugin/admin/src/components/MyCustomPlugin/index.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react'
 
-class MycustomPlugin extends Component {
-    state = {
-        file: null,
-        type: null,
-        options: {
-            filename: null
-        }
-    };
+import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-    onChangeImportFile = file => {
-        file &&
-        this.setState({
-            file,
-            type: file.type,
-            options: { ...this.state.options, filename: file.name }
-        });
-        console.log(this.state)
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
+        );
     }
-
-    render(){
-        // return <div><input name="file_input" accept=".csv" type="file" /></div>
-        return <input onChange={
-            ({target:{files}}) => files && this.onChangeImportFile(files[0])} name="file_input" accept=".csv" type="file" />;
-        
-    }
-
 }
-export default MycustomPlugin
+
+export default App;
