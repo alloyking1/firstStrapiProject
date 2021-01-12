5573585062025bde535144dab0c1400b631e8dbf
alloyking1 <alloyking1@gmail.com>
2021-01-12 06:29:19 -0500

    added Ckeditor to project

--- plugins/my-custom-plugin/admin/src/containers/HomePage/index.js
+++ plugins/my-custom-plugin/admin/src/containers/HomePage/index.js
@@ -12,8 +12,9 @@ import MyCustomPlugin from '../../components/MyCustomPlugin'
 const HomePage = () => {
   // return (
   //   <div>
-  //     <h1>{pluginId}&apos;s HomePage</h1>
-  //     <p>Happy coding</p>
+  //     {/* <h1>{pluginId}&apos;s HomePage</h1> */}
+  //     <h1>I changed my title</h1>
+  //     <p>Hi! I just earned my self a bragging right for chaning the title of this plugin</p>
   //   </div>
   // );
   return <MyCustomPlugin/>;
