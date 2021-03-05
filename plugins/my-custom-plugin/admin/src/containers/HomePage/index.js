d97e2121a20022abd819e306808644cb754f3732
alloyking1 <alloyking1@gmail.com>
2021-03-05 06:57:50 -0500

    updated some features, simplified codebase

--- plugins/my-custom-plugin/admin/src/containers/HomePage/index.js
+++ plugins/my-custom-plugin/admin/src/containers/HomePage/index.js
@@ -4,20 +4,12 @@
  *
  */
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
 import pluginId from '../../pluginId';
 import MyCustomPlugin from '../../components/MyCustomPlugin'
 
 const HomePage = () => {
-  // return (
-  //   <div>
-  //     {/* <h1>{pluginId}&apos;s HomePage</h1> */}
-  //     <h1>I changed my title</h1>
-  //     <p>Hi! I just earned my self a bragging right for chaning the title of this plugin</p>
-  //   </div>
-  // );
   return <MyCustomPlugin/>;
 };
 
-export default memo(HomePage);
+export default HomePage;
